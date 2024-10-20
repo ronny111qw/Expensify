a6632da002d6331144cdc51eaa9a03ba94a9048e
ronny11qw <mohammedkhan9271@gmail.com>
2024-10-21 02:37:20 +0530

    Updated config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      // This setting allows production builds to successfully complete even if there are ESLint errors
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
